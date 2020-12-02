e7a29abc7016a2414a3382b59073d39a69e23a2d
zw1983 <zw19831230@qq.com>
2020-12-02 11:33:29 +0800

    项目初始化调整目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
+  <!--单页面的绑定元素，id和main.js中$mount('#app')的id对应-->
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!--加一个默认文字-->
+    <h1>黑马头条内容发布系统</h1>
+    <!--routerview 默认路由入口-->
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
